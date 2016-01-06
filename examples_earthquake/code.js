335f9d6ce00ceaa9eadea0de6b9b44633be21c5a
Kyle Kelley <rgbkrk@gmail.com>
2016-01-06 16:53:13 -0600

    flatMap responses

--- examples_earthquake/code.js
+++ examples_earthquake/code.js
@@ -1,11 +1,13 @@
 var quakes = Rx.Observable.create((observer) => {
   window.eqfeed_callback = (response) => {
-    response.features.forEach((quake) => {
-      observer.onNext(quake);
-    });
+    observer.onNext(response);
+    observer.onCompleted();
   }
   loadJSONP(QUAKE_URL);
-});
+}).flatMap((dataset) => {
+  return Rx.Observable.from(dataset.features);
+})
+
 quakes.subscribe((quake) => {
   var coords = quake.geometry.coordinates;
   var size = quake.properties.mag * 10000;
