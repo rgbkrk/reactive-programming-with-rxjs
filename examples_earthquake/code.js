e794060585af9452d99c610c66ac9623ed3fdb7c
Kyle Kelley <rgbkrk@gmail.com>
2016-01-06 17:06:28 -0600

    Poll for new earthquake data and update the map

--- examples_earthquake/code.js
+++ examples_earthquake/code.js
@@ -1,17 +1,22 @@
-var quakes = Rx.DOM.jsonpRequest({
-  url: QUAKE_URL,
-  jsonpCallback: 'eqfeed_callback',
-})
-.flatMap((dataset) => {
-  return Rx.Observable.from(dataset.response.features);
-})
-.map((quake) => {
-  return {
-    lat: quake.geometry.coordinates[1],
-    lng: quake.geometry.coordinates[0],
-    size: quake.properties.mag * 10000,
-  };
-})
+var quakes = Rx.Observable
+  .interval(5000)
+  .flatMap(() => {
+    return Rx.DOM.jsonpRequest({
+      url: QUAKE_URL,
+      jsonpCallback: 'eqfeed_callback',
+    }).retry(3);
+  })
+  .flatMap((result) => {
+    return Rx.Observable.from(result.response.features);
+  })
+  .distinct(quake => quake.properties.code)
+  .map((quake) => {
+    return {
+      lat: quake.geometry.coordinates[1],
+      lng: quake.geometry.coordinates[0],
+      size: quake.properties.mag * 10000,
+    };
+  })
 
 quakes.subscribe((quake) => {
   L.circle([quake.lat, quake.lng], quake.size).addTo(map);
